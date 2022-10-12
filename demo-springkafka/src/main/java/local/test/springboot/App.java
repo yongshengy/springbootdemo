c523f49814d30355da2825a22cb346d5f7e192c7
Yongsheng Yang <yyang@talend.com>
2022-10-13 02:34:31 +0800

    small fix

---
+++ demo-springkafka/src/main/java/local/test/springboot/App.java
@@ -0,0 +1,13 @@
+package local.test.springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
